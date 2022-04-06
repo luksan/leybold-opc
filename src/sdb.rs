9844dbbd024d2f6a7902188ccdc53f1be8041346
Lukas Sandström <lukas.sandstrom@chalmers.se>
2022-04-06 09:57:59 +0200

    SDB: remove the ValueType enum and replace with a TypeDescr index

--- src/sdb.rs
+++ src/sdb.rs
@@ -115,7 +115,7 @@ impl BinRead for TypeDescPayload {
 #[br(little, magic = 0x05u32)]
 pub struct Parameter {
     len: u32,
-    pub value_type: ValueType, //ValueType,
+    pub type_descr_idx: u32,
     pub i2a: u16,
     pub i2: u16,
     pub i3: u32,
@@ -123,65 +123,12 @@ pub struct Parameter {
     pub name: SdbStr,
 }
 
-#[derive(BinRead, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-#[br(little, repr=u32)]
-#[repr(u32)]
-#[allow(non_camel_case_types)]
-pub enum ValueType {
-    Bool = 0x00,
-    I16 = 0x01,
-    Bstr = 0x02,
-    BstrUnit = 0x03,
-    BstrPrefix = 0x04,
-    F32 = 0x05,
-    BstrValue = 0x06,
-    UI1Array = 0x07,
-    x8 = 0x08,
-    x9 = 0x09,
-    x10 = 0x0a,
-    x11 = 0x0b,
-    x12 = 0x0c,
-    x13 = 0x0d,
-    x14 = 0x0e,
-    x15 = 0x0f,
-    x16 = 0x10,
-    x17 = 0x11,
-    x18 = 0x12,
-    x19 = 0x13,
-    x20 = 0x14,
-    x21 = 0x15,
-    x22 = 0x16,
-    x23 = 0x17,
-    x24 = 0x18,
-    ParamGrp = 0x19,
-    GaugeEntry = 0x1a,
-    Gauges = 0x1b,
-    Special = 0x1c,
-    x29 = 0x1d,
-    x30 = 0x1e,
-    x31 = 0x1f,
-    x32 = 0x20,
-    x33 = 0x21,
-    x34 = 0x22,
-    x35 = 0x23,
-    x36 = 0x24,
-    x37 = 0x25,
-    x38 = 0x26,
-    x39 = 0x27,
-    x40 = 0x28,
-    x41 = 0x29,
-    x42 = 0x2a,
-    x43 = 0x2b,
-    x44 = 0x2c,
-    x45 = 0x2d,
-}
-
 impl Debug for Parameter {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{:38?} id: {:05x}, type: {:?},\t i2a: {:x}, i2: {:x}, i3: {:x}",
-            self.name, self.id, self.value_type, self.i2a, self.i2, self.i3
+            self.name, self.id, self.type_descr_idx, self.i2a, self.i2, self.i3
         )
     }
 }
@@ -237,39 +184,6 @@ pub fn print_sdb_file() -> Result<()> {
     Ok(())
 }
 
-pub fn x04_analysis() -> Result<()> {
-    let sdb = read_sdb_file()?;
-
-    let mut x04 = sdb.type_descr.iter().collect::<Vec<_>>();
-
-    // x04.sort_by_key(|x| x.i1);
-    for e in x04.iter() {
-        let idx = e.type_idx;
-        let name = e.description.try_as_str()?;
-        let i1 = e.kind;
-        let type_size = e.type_size;
-        println!("X04:{idx:2x} kind {i1:?}, tlen {type_size:5x}, {name}");
-        match &e.payload {
-            TypeDescPayload::None => {}
-            TypeDescPayload::Array(a) => {
-                println!("  {a:?}")
-            }
-            TypeDescPayload::Pointer(p) => {
-                println!("  Pointer -> {p:x}")
-            }
-            TypeDescPayload::Struct(b) => {
-                for b in b.iter() {
-                    println!("  {b:x?}")
-                }
-            }
-            TypeDescPayload::Other(vec) => {
-                println!("{}", hexdump(vec))
-            }
-        }
-    }
-    Ok(())
-}
-
 #[binread]
 #[derive(Debug, Clone)]
 #[br(little)]
@@ -286,7 +200,7 @@ pub struct ArrayDesc {
 #[br(little, magic = 0x05u32)]
 pub struct StructMember {
     i1: u32,
-    value_type: ValueType,
+    type_descr_idx: u32,
     i: [u32; 2],
     id_offset: u32, // the number to add to this parameters id to get the sub entries id.
     name: SdbStr,
